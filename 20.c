1a23f734d0bdc67280b9e209476f6277919464ab
thilgapalani-tech <thilgapalani@gmail.com>
2026-03-11 12:02:41 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Welcome to C Programming!");
+    int a, b;
+    int sum, sub, mul;
+    float div;
+
+    printf("===== SIMPLE CALCULATOR =====\n");
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    sum = a + b;
+    sub = a - b;
+    mul = a * b;
+    div = (float)a / b;
+
+    printf("\nResults:\n");
+    printf("Addition = %d\n", sum);
+    printf("Subtraction = %d\n", sub);
+    printf("Multiplication = %d\n", mul);
+    printf("Division = %.2f\n", div);
+
+    printf("\nThank you for using the calculator!\n");
+
     return 0;
 }
